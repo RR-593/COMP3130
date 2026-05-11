50bce40424ff20152665de0b63a4bb88d4b31b72
Raphael Munoz <raphael@munoz.to>
2026-05-11 15:55:27 +1000

    firebse confige --android

--- trustmee_app/lib/main.dart
+++ trustmee_app/lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:trustmee_app/routes/app_routes.dart';
 import 'package:trustmee_app/theme/app_theme.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   await dotenv.load(fileName: '.secrets/maptiler_api');
   runApp(const TrustMeeApp());
